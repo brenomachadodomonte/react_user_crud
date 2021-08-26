a5126c0e53d7be7089c327425733c85897fbf589
Breno Machado do Monte <brenomachadodomonte@gmail.com>
2021-08-26 10:25:03 -0300

    Creating Home Component

---
+++ frontend/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Main from '../template/Main';
+
+export default props =>
+    <Main icon="home" title="Home" subtitle="CRUD with React.JS">
+        <div className="display-4">Welcome</div>
+        <hr/>
+        <p className="mb-0">A Web application for Create, Read, Update and Delete Users made with Reac.JS</p>
+    </Main>
